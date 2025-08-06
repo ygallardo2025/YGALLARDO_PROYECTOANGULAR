6d71b1c6b9ca90609dea9c18c9771c543aaf9154
ygallardo2025 <ygallardo@emaresa.cl>
2025-08-05 22:09:11 -0400

    feat: add courses module with CRUD

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CoursesManagerComponent } from './courses/courses-manager/courses-manager';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'cursos', component: CoursesManagerComponent },
+];

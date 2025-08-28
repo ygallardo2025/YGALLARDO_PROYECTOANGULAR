054dc2aafdb3ff58248309d3931f549cadedcf41
Yair Gallardo <ygallardo@emaresa.cl>
2025-08-28 17:02:24 -0400

    add Dist folder

--- dist/proyectoAngular_YGS/server/angular-app-manifest.mjs
+++ dist/proyectoAngular_YGS/server/angular-app-manifest.mjs
@@ -13,54 +13,54 @@ export default {
   {
     "renderMode": 2,
     "preload": [
-      "chunk-LTPY3PVS.js",
-      "chunk-F6YX5B72.js"
+      "chunk-4CHQAMRJ.js",
+      "chunk-CF6JDDZ2.js"
     ],
     "route": "/login"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-2GMSCEHN.js"
+      "chunk-4JEID35C.js"
     ],
     "route": "/dashboard"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-L6F76ND2.js",
-      "chunk-Z2VTDOTZ.js",
-      "chunk-HYBGBYM6.js",
-      "chunk-F6YX5B72.js"
+      "chunk-XJGKIM3J.js",
+      "chunk-ZQGHR2R6.js",
+      "chunk-V6KXUH27.js",
+      "chunk-CF6JDDZ2.js"
     ],
     "route": "/students"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-JZ6WBDA3.js",
-      "chunk-5IFMELBI.js",
-      "chunk-HYBGBYM6.js",
-      "chunk-F6YX5B72.js"
+      "chunk-XL6HBKNT.js",
+      "chunk-J7TO2JFV.js",
+      "chunk-V6KXUH27.js",
+      "chunk-CF6JDDZ2.js"
     ],
     "route": "/cursos"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-Z4HYK4FW.js",
-      "chunk-Z2VTDOTZ.js",
-      "chunk-5IFMELBI.js",
-      "chunk-HYBGBYM6.js",
-      "chunk-F6YX5B72.js"
+      "chunk-7EB5LE5N.js",
+      "chunk-ZQGHR2R6.js",
+      "chunk-J7TO2JFV.js",
+      "chunk-V6KXUH27.js",
+      "chunk-CF6JDDZ2.js"
     ],
     "route": "/inscripciones"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-3Q2UMSYH.js",
-      "chunk-F6YX5B72.js"
+      "chunk-6RNT7WMS.js",
+      "chunk-CF6JDDZ2.js"
     ],
     "route": "/usuarios"
   },
@@ -72,14 +72,14 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 34214, hash: '30bf8b759b72af3a3a00150227add973190eb0ceabe1f3bde152ea5c4387ca9d', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17532, hash: 'f038a6c006d03ccd959a4d41d03bbe6e8d4a8b772d85a8ba369f856eb9b60105', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'students/index.html': {size: 56884, hash: '5f9c56f736a3e5f1a764554713517f752621e2c7f03b88cbad974db35f45a016', text: () => import('./assets-chunks/students_index_html.mjs').then(m => m.default)},
-    'login/index.html': {size: 56780, hash: '70ded0695e2b2cb4ae00398684683bc3374174d5bf623660c06c9adbe46233f9', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
-    'dashboard/index.html': {size: 56728, hash: '3a2fd6e3e106b0ac6ebf120d7f55edba3cc2a73c45d1a4b844cc513d7d13461e', text: () => import('./assets-chunks/dashboard_index_html.mjs').then(m => m.default)},
-    'cursos/index.html': {size: 56884, hash: '77f406ea8eb78f0cc76d49daf3e90577b135f2cbee11420a6ef361a4329209aa', text: () => import('./assets-chunks/cursos_index_html.mjs').then(m => m.default)},
-    'inscripciones/index.html': {size: 56936, hash: '87774a353f17342e0d5a3d9b38359ccfcec8eb7680d15055a7b419f561580ab5', text: () => import('./assets-chunks/inscripciones_index_html.mjs').then(m => m.default)},
-    'usuarios/index.html': {size: 56780, hash: '53e112512f4aef5c35e4e23b71a7d900bce8de0eb5fb60ac7197e2a50bda9abb', text: () => import('./assets-chunks/usuarios_index_html.mjs').then(m => m.default)},
-    'styles-4GZ464VC.css': {size: 405393, hash: 't4iF+L6ir+s', text: () => import('./assets-chunks/styles-4GZ464VC_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 34255, hash: '65def0ba68449473105359e71785a1593b91e4836ab01b2c61463da118e70125', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17532, hash: '8b64868abb7325b7299f46d39a038ec537922b0f2b4ddaf70fcc6775b2d5b746', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'dashboard/index.html': {size: 53934, hash: '510dcc1c12ed446919e509669b1d803a2c72b694ac9ac0d578d712c96fbd9e09', text: () => import('./assets-chunks/dashboard_index_html.mjs').then(m => m.default)},
+    'usuarios/index.html': {size: 53986, hash: '263255d622109107cd159566196ec5dd1bbaa204e019405f0a5ea9bb70c29ac7', text: () => import('./assets-chunks/usuarios_index_html.mjs').then(m => m.default)},
+    'students/index.html': {size: 54090, hash: '8ca5c4bf1f8d7d1fad2c5b15fe5c16e20ba491b719f44a84ca2995f0c4b9526d', text: () => import('./assets-chunks/students_index_html.mjs').then(m => m.default)},
+    'login/index.html': {size: 53986, hash: '95a09032ac6ff3e7bbc0e99681c73d0b9e47434dcba17fef6e849472bf356006', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
+    'cursos/index.html': {size: 54090, hash: 'cfe827181c14ec2abbadf07c7aea0c6e361102c1d4f8be373c7e35a746b942c0', text: () => import('./assets-chunks/cursos_index_html.mjs').then(m => m.default)},
+    'inscripciones/index.html': {size: 54142, hash: 'f973dcc9e6bb2b7cc2e5de5b90b749a3bcbda9f7abc7a37d738afbbf469c2c46', text: () => import('./assets-chunks/inscripciones_index_html.mjs').then(m => m.default)},
+    'styles-2LIKKSNB.css': {size: 405436, hash: 'bVlBXIWPlHg', text: () => import('./assets-chunks/styles-2LIKKSNB_css.mjs').then(m => m.default)}
   },
 };
